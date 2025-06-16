23167852a0b1143a97feadd767828699584bed20
Cory Francis Myers <cory@freedom.press>
2025-06-16 14:27:50 -0700

    chore: placeholder for Journalist API v2 specification

---
+++ securedrop/journalist_app/api2.py
@@ -0,0 +1,19 @@
+# This file is a placeholder for a lightweight specification of a v2 Journalist
+# API that implements the synchronization strategy proposed in "PROPOSAL.md".
+# The specification should include:
+#
+# 1. a semi-literate[^1] reference implementation in Python of the algorithms
+#    for versioning and diffing resources;
+#
+# 2. an initial set of test vectors that can be used to test another
+#    implementation in another language (e.g., TypeScript :-); and
+#
+# 3. stubs (i.e., signatures that raise NotImplementedError) for the endpoints
+#    the new API will provide.
+#
+# By "semi-literate" I mean that this file should (a) be self-sufficient for
+# understanding the API's synchronization strategy and (b) produce reasonable
+# output under a documentation generator like Doxygen or Sphinx.
+#
+#
+# [^1]: https://en.wikipedia.org/wiki/Literate_programming
